927ef24738166aa8fb8ec76aaf7d0f492b4ba41b
RyamL1221 <ryaml1221@gmail.com>
2024-03-23 10:52:28 -0400

    first commit

---
+++ HackRU24SBack/src/main/java/HackRU24S/HackRu24SApplication.java
@@ -0,0 +1,13 @@
+package HackRU24S;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackRu24SApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackRu24SApplication.class, args);
+	}
+
+}
---
+++ HackRU24SBack/src/test/java/HackRU24S/HackRu24SApplicationTests.java
@@ -0,0 +1,13 @@
+package HackRU24S;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackRu24SApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
