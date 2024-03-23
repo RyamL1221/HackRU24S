39400ee23adcd8743fe657bc4b2b47a89fba04b8
RyamL1221 <ryaml1221@gmail.com>
2024-03-23 12:27:44 -0400

    added environments and task interface and task service

---
+++ HackRU24SFront/src/app/task-list/task.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { environment } from "src/environment/environment";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from "rxjs";
+import { Task } from "./task";
+
+@Injectable({providedIn: 'root'})
+export class TaskService {
+    private apiServerUrl = environment.apiBaseUrl;
+
+    constructor(private http: HttpClient) {}
+
+    public getTasks(): Observable<Task[]> {
+        return this.http.get<Task[]>(`${this.apiServerUrl}/task/all`);
+    }
+
+    public addTask(task: Task): Observable<Task> {
+        return this.http.post<Task>(`${this.apiServerUrl}/task/add`, task);
+    }
+
+    public updateTask(task: Task): Observable<Task> {
+        return this.http.put<Task>(`${this.apiServerUrl}/task/update`, task);
+    }
+
+    public deleteTask(taskId: String): Observable<void> {
+        return this.http.delete<void>(`${this.apiServerUrl}/task/delete/${taskId}`);
+    }
+}
